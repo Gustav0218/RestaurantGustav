6e29100848a820c21d0651ab08d8ad3a13501a1a
Gustavo Ornelas <ornelasg93@gmail.com>
2023-01-05 10:37:10 -0600

    Se agrega favicon/adress/modificaciones footer-header.

--- src/Components/shared/footer.js
+++ src/Components/shared/footer.js
@@ -1,21 +1,40 @@
 const Footer = () => {
-  return(
+  return (
     <>
-    
-<div className="footer-cont">
-  <footer class="py-3 my-4">
-    <ul class="nav justify-content-center border-bottom pb-3 mb-3">
-      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Home</a></li>
-      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Features</a></li>
-      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Pricing</a></li>
-      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">FAQs</a></li>
-      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">About</a></li>
-    </ul>
-    <p class="text-center text-muted">&copy; Proyecto UCAMP 2022</p>
-  </footer>
-</div>
-  </>
-    )
-  };
-  
-  export default Footer;
+      <div className="footer-cont">
+        <footer class="py-3 my-4">
+          <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+            <li class="nav-item">
+              <a href="/" class="nav-link px-2 text-muted">
+                Inicio
+              </a>
+            </li>
+            <li class="nav-item">
+              <a href="contactanos" class="nav-link px-2 text-muted">
+                Contacto
+              </a>
+            </li>
+            <li class="nav-item">
+              <a href="direccion" class="nav-link px-2 text-muted">
+                Donde encontrarnos
+              </a>
+            </li>
+            <li class="nav-item">
+              <a href="/" class="nav-link px-2 text-muted">
+                FAQs
+              </a>
+            </li>
+            <li class="nav-item">
+              <a href="*" class="nav-link px-2 text-muted">
+                Acerca de nosotros
+              </a>
+            </li>
+          </ul>
+          <p class="text-center text-muted">&copy; Proyecto UCAMP 2022</p>
+        </footer>
+      </div>
+    </>
+  );
+};
+
+export default Footer;
--- src/Components/shared/header.js
+++ src/Components/shared/header.js
@@ -8,9 +8,9 @@ const Header = () => {
         <Container>
           <Navbar.Brand href="#home">Bienvendo a Tu Restaurante</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <Nav.Link href="/">Inicio</Nav.Link>
+            <Nav.Link href="direccion">Contacto</Nav.Link>
+            <Nav.Link href="*">Precios</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
